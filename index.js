3f472c4da573c143c511c9f72be9850b651a85bd
Milos Zivadinovic <mzdv@outlook.com>
2017-03-12 18:44:53 +0100

    Implement main menu.

---
+++ index.js
@@ -0,0 +1,16 @@
+var menu = require('appendable-cli-menu');
+var chalk = require('chalk');
+var bindings = require('./modules/bindings');
+
+console.log(chalk.cyan('--------------------------------'));
+console.log(chalk.magenta('Aplikacija iz Baza podataka 2'));
+console.log(chalk.magenta('Milos Zivadinovic 3028/16 2017'));
+console.log(chalk.cyan('--------------------------------'));
+
+var appOptions = menu('Izaberite opciju', (option) => {
+    option.activate();
+});
+
+bindings.forEach((binding) => {
+    appOptions.add(binding);
+});
---
+++ modules/bindings/index.js
@@ -0,0 +1,33 @@
+module.exports = [
+    {
+        name: 'Izlaz',
+        activate: function () {
+            console.log('Izlazim...');
+            process.exit();
+        }
+    },
+    {
+        name: 'Denormalizacija 2NF',
+        activate: function () { }
+    },
+    {
+        name: 'Denormalizacija 3NF',
+        activate: function () { }
+    },
+    {
+        name: 'Struktuirani tip',
+        activate: function () { }
+    },
+    {
+        name: 'Distinct tip',
+        activate: function () { }
+    },
+    {
+        name: 'Derivable values',
+        activate: function () { }
+    },
+    {
+        name: 'Vertikalno particionisanje',
+        activate: function () { }
+    }
+];
